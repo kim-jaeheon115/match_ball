c82e8fb33b67241671871fcac28659e677a73d01
[github <[github>
2023-11-23 12:32:49 +0900

    11월 23일 이름 나이까지 구현// 데이터베이스 marginleft까지 하고 담부터이어서 ㄱㄱ

---
+++ app/src/main/java/com/example/matchball/User.java
@@ -0,0 +1,62 @@
+package com.example.matchball;
+
+public class User {
+    private String profile;
+    private String id;
+    private int pw;
+    private String userName;
+    private int age;
+    private String pNumber;
+
+    public User(){
+
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getPw() {
+        return pw;
+    }
+
+    public void setPw(int pw) {
+        this.pw = pw;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getpNumber() {
+        return pNumber;
+    }
+
+    public void setpNumber(String pNumber) {
+        this.pNumber = pNumber;
+    }
+}
